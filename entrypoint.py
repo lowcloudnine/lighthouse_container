2bb0e29ea3981f38a5b3289db3eab06d6532b7ee
Jon Schiefelbein <lowcloudnine@user.noreply.github.com>
2022-04-26 13:10:14 -0400

    clean up the argparse functionality

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,7 @@
 """
 Entrypoint
 ----------
+
 A short script to parse the "score" from the JSON output of Google Lighthouse and
 to work as the entry point for a docker container.
 """
@@ -10,7 +11,9 @@ to work as the entry point for a docker container.
 
 
 # ---- System
+import argparse
 import json
+import os
 import subprocess
 import sys
 
@@ -19,108 +22,115 @@ import sys
 # ----------------------------------------------------------------------------
 
 
-def display_help():
-    """Provide a helpful well formatted message for users."""
-    the_help = """
-Welcome to the "lighthouse container"!
-The goal is to be a simple interface to automate generating tests from
-Google's Lighthouse testing/scoring application.  The container prints a
-single integer to stdout from a given URL and category of tests to run.
-URL should be the full URL including protocol examples:
-- http://google.com
-- https://redhat.com
-The category must be one of:
-- accessibility
-- best-practices
-- performance
-- pwa
-- seo
-Example usage:
-CLI with output:
-- $ docker run lighthouse accessibility https://redhat.com
-  91
-- $ docker run lighthouse best-practices https://redhat.com
-  75
-In a bash script:
-- SCORE=$(docker run lighthouse acessibility https://redhat.com)
-"""
-    print(the_help)
-
-
-def lighthouse(url, category, report_type="json"):
+def lighthouse(url, category, report):
     """Use lighthouse to generate an output file."""
-    report_file = f"/output/output.{report_type}"
-
     # All the options for lighthouse can be found at:
     #  https://github.com/GoogleChrome/lighthouse#cli-options
+    chrome_flags = " ".join([
+        "--headless",
+        "--window-size=821,843",
+        "--no-sandbox",
+        "--enable-javascript",
+        "--ignore-certificate-errors",  # ignore https certificate issues
+    ])
+
     lighthouse_args = [
         url,
         # chrome flags are needed to fake out sites as headless often makes them
         # not provide the data needed
-        '--chrome-flags="--headless --window-size=821,843 --no-sandbox --enable-javascript"',
+        f'--chrome-flags="{chrome_flags}"',
         f'--only-categories "{category}"',
-        '--output json',
-        f'--output-path {report_file}',
         '--no-enable-error-reporting',  # this keeps Google's request for reports quiet
                                         # and avoids the associated 20 second delay
         '--quiet'  # Keeps the lighthouse output quiet comment out if you are debugging
     ]
 
-    lhouse = subprocess.run(
+    if report:
+        lighthouse_args.append('--output html')
+        lighthouse_args.append('--output-path /output/output.html')
+    else:
+        lighthouse_args.append('--output json')
+        lighthouse_args.append('--output-path /output/output.json')
+
+    subprocess.run(
         " ".join(["lighthouse", *lighthouse_args]),
         shell=True, check=True,
         stdout=subprocess.PIPE
     )
 
 
-def parse_score(category, report_type="json"):
+def parse_score(category, report):
     """Entry point for the script."""
-    report_file = f"/output/output.{report_type}"
+    report_json = "/output/output.json"
 
-    with open(report_file, "r") as report:
+    with open(report_json, "r", encoding="utf-8") as report:
         output = json.load(report)
     score = int(float(output['categories'][category]['score']) * 100)
 
+    # Clean up the file so if there's a volume mount, there's no clutter
+    os.remove("/output/output.json")
+
     return str(score)
+
+
+def parse_args():
+    """Return a namespace of the parsed arguments for the program."""
+    parser = argparse.ArgumentParser(
+        prog="docker run [various docker options] lighthouse",
+        description='Run Google Lighthouse inside a docker container with some options.'
+    )
+    parser.add_argument(
+        '--category', '-c',
+        type=str,
+        default='accessibility',
+        choices=[
+            "accessibility",
+            "best-practices",
+            "performance",
+            "pwa",
+            "seo"
+        ],
+        help="The category/type of test to run, defaults to accessibility."
+    )
+    parser.add_argument(
+        '--url', '-u',
+        type=str,
+        default='https://python.org',
+        help="A valid URL to a web site to test must start with a valid protocol " \
+             "of http:// or https://.  Defaults to https://www.python.org"
+    )
+    parser.add_argument(
+        '--report', '-r',
+        action='store_true',
+        help="Generate/Output the full HTML report to a file.  If this flag/option " \
+             "is set only the html file will be generated in the directory mounted " \
+             "with the docker volume flag, i.e. -v /home/user/reports:/output " \
+             "as output.html.  If the flag is not given a single number, the " \
+             "result of the given test which is between 0 and 100 will be piped " \
+             "to stdout."
+    )
+
+    all_args = parser.parse_args()
+    if not(all_args.url.startswith("http://")
+            or all_args.url.startswith("https://")):
+        print("ERROR: Provide a valid URL, it must start with: http:// or https://")
+        sys.exit(1)
+
+    return all_args
 
 # ----------------------------------------------------------------------------
 # Main
 # ----------------------------------------------------------------------------
 
 
-def main(category, url):
+def main(category, url, report):
     """Run as the entrypoint and ensure the input is correct."""
-    help_opts = ["help", "--help", "-h", "h"]
-    if category in help_opts \
-        or url in help_opts:
-        display_help()
-        sys.exit(0)
-
-    valid_categories = [
-        "accessibility",
-        "best-practices",
-        "performance",
-        "pwa",
-        "seo"
-    ]
-    if category not in valid_categories:
-        print(f'ERROR:  Category "{category}" is not valid.')
-        display_help()
-        sys.exit(1)
-
-    if url == "":
-        print("ERROR:  Please provide a URL to run against.")
-        display_help()
-        sys.exit(2)
-
-    if not( url.startswith("http://") or url.startswith("https://")):
-        print(f"ERROR:  {url} does not start with a valid protocol.")
-        display_help()
-        sys.exit(3)
-
     # Run Google Lighthouse for the given category and URL
-    lighthouse(url, category)
-    print(parse_score(category))  # <-- print the score to stdout
+    lighthouse(url, category, report)
+    if report:
+        print("The results are in: ./output.html")
+    else:
+        print(parse_score(category, report))  # <-- print the score to stdout
 
 # ----------------------------------------------------------------------------
 # Name
@@ -128,10 +138,5 @@ def main(category, url):
 
 
 if __name__ == "__main__":
-    category = sys.argv[1]
-    try:
-        url = sys.argv[2]
-    except IndexError:
-        url = ""
-
-    main(category, url)
+    the_args = parse_args()
+    main(the_args.category, the_args.url, the_args.report)
